cba8642419a2aabeb96b3bfab5ea706d136dc711
Feilong Ma <mafeilong@gmail.com>
2014-03-16 00:45:07 -0400

    org babel for R

--- .emacs.d/org-config.el
+++ .emacs.d/org-config.el
@@ -1,8 +1,10 @@
 (setq org-startup-indented t)
+(require 'ess-site)
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)
    (sh . t)
+   (R . t)
   )
 )
 (setq org-src-fontify-natively t)
